0c488dad835d5338fe734d7f88961c3bc38c671c
DegasiPilot <Timurnig@gmail.com>
2025-07-08 17:59:03 +0300

    Fix and add tests

--- CheckURI/Program.cs
+++ CheckURI/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        System.Diagnostics.Debug.WriteLine("UriHelper tests result: " + URIHelper.RunTests());
+        Console.ReadKey();
     }
 }
--- CheckURI/URIHelper.cs
+++ CheckURI/URIHelper.cs
@@ -10,20 +10,81 @@ namespace CheckURI
         };
 
 
-        static bool CheckUri(string uri)
+        public static bool CheckUri(Uri uri)
         {
-            if (string.IsNullOrEmpty(uri))
+            if (uri is null)
                 return false;
 
             foreach (var pattern in allowedPatterns)
             {
-                if (uri.EndsWith(pattern))
+                if (uri.AbsoluteUri.Contains(pattern))
                 {
                     return true;
                 }
             }
 
             return false;
+        }
+
+        public static bool RunTests()
+        {
+            // Прямое совпадение
+            if (!CheckUri(new Uri("https://test.tatneft.ru")) ||
+                !CheckUri(new Uri("https://sub.test.tatneft.ru")) ||
+                !CheckUri(new Uri("http://example.tatneft.tatar")))
+            {
+                return false;
+            }
+
+            // С завершающим слешем
+            if (!CheckUri(new Uri("https://test.tatneft.ru/")) ||
+                !CheckUri(new Uri("https://sub.test.tatneft.ru/path")) ||
+                !CheckUri(new Uri("http://example.tatneft.tatar/")))
+            {
+                return false;
+            }
+
+            // Несовпадающие домены
+            if (CheckUri(new Uri("https://test.tatneft.com")) || // другой домен
+                CheckUri(new Uri("https://example.tateft.tatar"))) // опечатка
+            {
+                return false;
+            }
+
+            // Другой URL
+            if (CheckUri(new Uri("https://google.com")))
+            {
+                return false;
+            }
+
+            // Другой протокол
+            if (!CheckUri(new Uri("ftp://test.tatneft.ru")))
+            {
+                return false;
+            }
+
+            // Пустой URI
+            if (CheckUri(null))
+            {
+                return false;
+            }
+
+
+            // С параметрами запроса
+            if (!CheckUri(new Uri("https://test.tatneft.ru?param=value")) ||
+                !CheckUri(new Uri("https://test.tatneft.tatar/?query=123")))
+            {
+                return false;
+            }
+
+            // С указанием порта
+            if (!CheckUri(new Uri("https://test.tatneft.ru:8080")) ||
+                !CheckUri(new Uri("http://example.tatneft.tatar:1234/path")))
+            {
+                return false;
+            }
+
+            return true;
         }
     }
 }
